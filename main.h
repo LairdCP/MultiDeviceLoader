d066e9c0abdbedc5e083533c49fa6501d4612c07
thedjnK <spam@helper3000.net>
2015-10-04 19:50:38 +0100

    v1.01, add new device identifier function

--- main.h
+++ main.h
@@ -50,7 +50,8 @@
 #define PauseRate 100 //Lower = faster but will break slower baud rates. 50 for 9600, 20 for 115200
 #define MaxPorts 10 //Number of maximum ports to allow
 #define MaxFilenameLength 20 //Maximum allowable length of a filename (for the target BL600/BL620/BT900 device)
-#define AppVersion "v1.00" //Version string
+#define AppVersion "v1.01" //Version string
+#define MaxDevNameSize 8 //Size (in characters) to allow for a module device name (characters past this point will be chopped off)
 
 /******************************************************************************/
 // Global/Static Variable Declarations
@@ -83,6 +84,11 @@ main(
 void
 ClosePorts(
     unsigned char ucNumPorts
+    );
+
+QString
+AtiToXCompName(
+    QString strAtiResp
     );
 
 #endif // MAIN_H
